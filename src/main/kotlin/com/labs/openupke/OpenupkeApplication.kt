dc18358cf008925a5cccc6e505bb902d2085f1cc
gegohcomp@gmail.com <gegohcompgmail.com@users-MacBook-Pro.local>
2022-02-10 00:53:13 +0300

    set up

---
+++ src/main/kotlin/com/labs/openupke/OpenupkeApplication.kt
@@ -0,0 +1,11 @@
+package com.labs.openupke
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OpenupkeApplication
+
+fun main(args: Array<String>) {
+	runApplication<OpenupkeApplication>(*args)
+}
